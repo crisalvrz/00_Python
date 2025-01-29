74e28f3d53cf82f4faab2788c387b9a8e317c473
Cris Álvarez <167922974+crisalvrz@users.noreply.github.com>
2025-01-29 13:45:40 +0100

    Update 7_INDICE_MASA_CORPORAL.py

--- 7_INDICE_MASA_CORPORAL.py
+++ 7_INDICE_MASA_CORPORAL.py
@@ -1,5 +1,6 @@
 # CALCULADORA DE ÍNDICE DE MASA CORPORAL
 estatura =float(input("Escriba su estatura en metros: "))
 peso =float(input("Escriba su peso: "))
+imc=round((peso/(estatura*estatura)),2)
 
-print("Tu índice de masa corporal es: ", (peso/(estatura*estatura)))
+print("Tu índice de masa corporal es: ", imc)
