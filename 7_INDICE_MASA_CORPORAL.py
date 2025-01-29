91769eb8cbd8a7d9a74d51eec18bcdfd275c0224
Cris Álvarez <167922974+crisalvrz@users.noreply.github.com>
2025-01-29 08:48:46 +0100

    Add files via upload

---
+++ 7_INDICE_MASA_CORPORAL.py
@@ -0,0 +1,5 @@
+# CALCULADORA DE ÍNDICE DE MASA CORPORAL
+estatura =float(input("Escriba su estatura en metros: "))
+peso =float(input("Escriba su peso: "))
+
+print("Tu índice de masa corporal es: ", (peso/(estatura*estatura)))
